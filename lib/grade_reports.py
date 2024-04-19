c9753db4fdb97175be89a45e3ea1ef49f2dde133
Timothy Ryon <tim@timryon.com>
2024-04-19 13:32:17 -0400

    finish codealong

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
+def create_grade_report(student_grades):
+    with open('./reports/grade_report.txt', 'a') as gr:
+        for grade in student_grades:
+            # add '\n' to write grades on separate lines
+            gr.write(grade + '\n')
+
 if __name__ == '__main__':
-    pass
+    student_grades = []
+    
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        # end when no grade is entered
+        grade = input("Student name, grade: ")
+
+    create_grade_report(student_grades)
+
+# below only works for one entry at a time...
+# if __name__ == '__main__':
+#     student_grades = input("Student name, grade:")
+#     create_grade_report(student_grades)
